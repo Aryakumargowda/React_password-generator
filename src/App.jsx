50bdf9fe6300fd42eb76eb3a9c66ca3cf71d84e1
Arya Kumar <74587299+Aryakumargowda@users.noreply.github.com>
2023-09-13 00:50:29 +0530

    Style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useCallback,useEffect, useRef } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
@@ -7,50 +7,72 @@ function App() {
   const [num, setNum] = useState(false);
   const [char, setChar] = useState(false);
 
-  const passgen=useCallback(()=>{
-    let pas="";
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if(num) str+="1234567890"
-    if(char) str+="!@#$%^&*(){}[]"
-    for(let i=0;i<length;i++)
-    {
-      let ln=Math.floor(Math.random()*str.length+1)
-      pas+=str.charAt(ln);
+  const passgen = useCallback(() => {
+    let pas = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (num) str += "1234567890";
+    if (char) str += "!@#$%^&*(){}[]";
+    for (let i = 0; i < length; i++) {
+      let ln = Math.floor(Math.random() * str.length + 1);
+      pas += str.charAt(ln);
     }
     setPass(pas);
-  },[length,num,char])
+  }, [length, num, char]);
 
-  useEffect(()=>{passgen()},[length,num,char,passgen])
-  
-  const copy=useRef(null)
+  useEffect(() => {
+    passgen();
+  }, [length, num, char, passgen]);
 
-  const copyToClip=useCallback(()=>{
+  const copy = useRef(null);
+
+  const copyToClip = useCallback(() => {
     copy.current?.select();
     navigator.clipboard.writeText(`${pass}`);
-  },[pass])
-
+  }, [pass]);
 
   return (
-    <>
+    <div className="main">
       <h1>Password Generator</h1>
       <div className="contents">
         <div className="text">
-          <input type="text" value={pass} readOnly ref={copy}/>
-          <button onClick={copyToClip}>copy</button>
-        <div className="choice">
-          <input type="range"
-          min={8}
-          max={50}
-          value={length}
-          onChange={(e)=>{setLen(e.target.value)}}/><label>{length}</label>
-          <input type="checkbox"
-          onChange={()=>{setNum((prev)=>!prev)}}/><label>Numbers</label>
-          <input type="checkbox"
-           onChange={()=>{setChar((prev)=>!prev)}}/><label>Special character</label>
+          <input type="text" value={pass} readOnly ref={copy} className="pas" />
+          <button onClick={copyToClip} className="but">
+            copy
+          </button>
         </div>
+        <div className="choice">
+          <div className="sep">
+            <input
+              type="range"
+              min={8}
+              max={50}
+              value={length}
+              onChange={(e) => {
+                setLen(e.target.value);
+              }}
+            />
+            <label>{length}</label>
+          </div>
+          <div className="sep">
+          <input
+            type="checkbox"
+            onChange={() => {
+              setNum((prev) => !prev);
+            }}
+          />
+          <label>Numbers</label></div>
+          <div className="sep">
+          <input
+            type="checkbox"
+            onChange={() => {
+              setChar((prev) => !prev);
+            }}
+          />
+          <label>Special character</label>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
